fad78da5b24a6549077ee6e2d5ef19698bb98a1e
Marat <maratnasyrov97@gmail.com>
2025-01-17 17:55:06 +0300

    Add details section

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,17 @@
-console.log("Hello!");
+const accordionBodies = document.querySelectorAll(".details-accordion__body");
+
+accordionBodies.forEach((accordionBody, index) => {
+  const accordionButton = accordionBody.querySelector(
+    ".details-accordion__button"
+  );
+  const accordionPanel = accordionBody.querySelector(
+    ".details-accordion__panel"
+  );
+
+  accordionButton.addEventListener("click", () => {
+    const arrow = accordionButton.querySelector(".details-accordion__icon");
+    arrow.classList.toggle("details-accordion__icon-active");
+
+    accordionPanel.classList.toggle("details-accordion__panel-active");
+  });
+});
